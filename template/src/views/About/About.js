7e497df910631766c50c09cf9fd30da6618110aa
ouweiya <Hi-Outlok@outlook.com>
2019-12-25 18:47:09 +0800

    v2.1.1

--- template/src/views/About/About.js
+++ template/src/views/About/About.js
@@ -1,5 +1,5 @@
 import React, { useEffect, createElement as e } from 'react';
-import { Link, useLocation, Switch } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const About = props => {
   return e(
